a41757ae22cf70d4504a30eb60f8b86958b3e5c1
Apollox <ukapollox@gmail.com>
2024-10-03 13:52:03 +0200

    aufgabe

---
+++ Aufgabe_06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+
+Console.WriteLine("-----------");
+Console.WriteLine("Kleines 1x1");
+Console.WriteLine("-----------");
+
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.Write($"{i * j}");
+
+    }
+    Console.WriteLine();
+}
